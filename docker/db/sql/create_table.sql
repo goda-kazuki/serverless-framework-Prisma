fe5faf4401b981c4698e72af1d822a1e918c20fc
goda-kazuki <goda@eyemovic.com>
2022-09-14 09:29:34 +0900

    add cars table

--- docker/db/sql/create_table.sql
+++ docker/db/sql/create_table.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
-    id   int(5) unsigned zerofill NOT NULL AUTO_INCREMENT COMMENT 'ユーザー番号',
+    id   int         NOT NULL AUTO_INCREMENT COMMENT 'ユーザー番号',
     name varchar(64) NOT NULL COMMENT '名前',
     PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS cars;
+CREATE TABLE cars
+(
+    id      int unsigned zerofill NOT NULL AUTO_INCREMENT COMMENT '車番号',
+    user_id int         NOT NULL COMMENT 'オーナー番号',
+    name    varchar(64) NOT NULL COMMENT '名前',
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- docker/db/sql/insert_data.sql
+++ docker/db/sql/insert_data.sql
@@ -2,3 +2,8 @@ INSERT INTO users (name)
 VALUES ('太郎1'),
        ('太郎2'),
        ('太郎3');
+
+INSERT INTO cars (user_id, name)
+VALUES (1, '車1'),
+       (2, '車2'),
+       (3, '車3');
